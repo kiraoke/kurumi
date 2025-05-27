f2618043f07cc1692cc2d9e9fbc204d651decb73
kiraoke <kiraokeku@gmail.com>
2025-05-27 22:53:00 +0530

    switch client from deno to node
    because issues

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,16 @@
+import React from "react";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>kurumi tokisaki</div>
+    <div>
+      <h1>Kurumi.</h1>
+      <Image
+        src="/mi.gif"
+        height={80}
+        width={80}
+        alt="Picture of kurumi tokisaki"
+      />
+    </div>
   );
 }
