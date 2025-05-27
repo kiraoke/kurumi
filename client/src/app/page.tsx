9d402d015e6e0fc5581b9d58dfb829c49051d46b
kiraoke <kiraokeku@gmail.com>
2025-05-27 22:10:49 +0530

    Initialize nextjs client

---
+++ client/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>kurumi tokisaki</div>
+  );
+}
---
+++ client/src/utils/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+  "Ash gray": "c2cfb2",
+  "Space cadet": "2e294e",
+  "Carmine": "91171f",
+  "Thistle": "cebacf",
+  "Rich black": "07090f",
+};
+
+export const kurumiColors = {
+  "Imperial red": "FA3038",
+  "Indigo dye": "1E405D",
+  "Pale Dogwood": "E8CBC1",
+  "Orange (web)": "FAAA00",
+  "English Violet": "443444",
+  "Misty rose": "F5DFDC",
+  "Crimson": "E40044",
+};
