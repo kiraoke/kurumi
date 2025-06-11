8196097286a9c31edeb8fd0cceb8e42baff8a597
kiraoke <kiraokeku@gmail.com>
2025-06-11 07:17:03 +0530

    redirect from success auth page

--- client/src/app/auth/success/page.tsx
+++ client/src/app/auth/success/page.tsx
@@ -4,20 +4,23 @@ import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { useAtom } from "jotai";
 import { accessTokenAtom } from "@/state/store";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function SuccessPage() {
   const searchParams = useSearchParams();
   const accessToken = searchParams.get("accessToken");
   const [, setAccessToken] = useAtom(accessTokenAtom);
+  const router = useRouter();
 
-  useEffect(() => setAccessToken(accessToken), []);
+  useEffect(() => {
+    setAccessToken(accessToken);
+    router.push("/");
+  }, []);
 
   return (
     <div>
       <h1>Success!</h1>
       <p>Your action was successful.</p>
-      <Link href="/auth/fail">access</Link>
     </div>
   );
 }
